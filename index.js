abb6dbeb6f23631d41f0a0883ba386edb65027d9
Mitchell3514 <m.d.rademaker19@gmail.com>
2017-09-05 18:37:26 +0200

    Changed index

--- index.js
+++ index.js
@@ -7,7 +7,65 @@ client.on('ready', () => {
   console.log('I am ready!');
 });
 
-require('./public/commands.js')
+client.on('message', (message) => {
+
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.toLowerCase().split(' ')[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+
+  let channel = message.channel
+  let author = message.author
+  let server = message.guild
 
 
-client.login('MzUzODk3MjU2MTUzNTEzOTg3.DI2YHw.B6TM06NfP8nQulVZ7vtmiDmc95A');
+  if (command === 'nick' & message.author.id === '124989722668957700') {
+    client.user.setUsername(argresult)
+      .then(user => console.log(`My new username is ${user.username}`))
+      .catch(console.error);
+    };
+
+  if (command === 'rape') {
+    channel.send(`You just raped ${argresult}`)
+      .then(message => console.log(`send message: ${message.content}`))
+      .catch(console.error);
+  };
+
+  if (command === 'info' || command === 'help') {
+    channel.send('No embed yet so no help.')
+      .then(message => console.log(`send message: ${message.content}`))
+      .catch(console.error);
+  };
+
+});
+
+
+
+client.on('message', (message) => {
+
+  let command = message.content.toLowerCase().split(' ')[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+
+  let channel = message.channel
+
+  if(message.author.bot) return;
+
+  if (message.content.includes('ping')) {
+    channel.send('pong')
+  };
+
+  if (message.content.includes('pubg')) {
+    var pubg = ['ja', 'nee', 'kom zelf pubg', 'waarom?', 'wel met esref', 'okay', 'kom csgo']
+    channel.send(pubg[Math.floor(Math.random() *pubg.length)])
+  }
+
+});
+
+
